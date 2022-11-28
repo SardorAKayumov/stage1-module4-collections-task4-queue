701eb2685fc2c46d2a12e7826aca92dd8a64e5f6
SardorAKayumov <sardor.zeroz@gmail.com>
2022-11-28 17:21:33 +0500

    commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,25 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> firstDeque = new ArrayDeque<>(firstQueue);
+        ArrayDeque<Integer> secondDeque = new ArrayDeque<>(secondQueue);
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.offer(firstDeque.remove());
+        deque.offer(firstDeque.remove());
+        deque.offer(secondDeque.remove());
+        deque.offer(secondDeque.remove());
+
+        while (!firstDeque.isEmpty() && !secondDeque.isEmpty()) {
+            firstDeque.offer(deque.removeLast());
+            deque.offer(firstDeque.remove());
+            deque.offer(firstDeque.remove());
+
+            secondDeque.offer(deque.removeLast());
+            deque.offer(secondDeque.remove());
+            deque.offer(secondDeque.remove());
+        }
+
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,34 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            deque.add(i);
+        }
+
+        while (!deque.isEmpty()) {
+            List<Integer> tempList = new ArrayList<>();
+            int skipNum = everyDishNumberToEat;
+
+            if (everyDishNumberToEat > deque.size()) {
+                skipNum = everyDishNumberToEat - deque.size();
+                if (deque.size() == 1)
+                    skipNum = 1;
+            }
+
+            for (int i = 1; i < skipNum; i++) {
+                tempList.add(deque.remove());
+            }
+
+            list.add(deque.remove());
+            deque.addAll(tempList);
+        }
+
+        return list;
     }
 }
